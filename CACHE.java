cfa95256ef30729c0d7be086e41961db84b0e353
EricX003 <90646214+EricX003@users.noreply.github.com>
2021-11-21 14:58:57 -0600

    Delete CACHE.java

--- CACHE.java
+++
@@ -1,6 +0,0 @@
-//Implementation by Eric Xing
-
-public interface CACHE { // Implementation of a cache replacement policy
-	void add(int value); // Insert a value into the cache
-	int get(int value); // Query if a value is present in the cache
-}
